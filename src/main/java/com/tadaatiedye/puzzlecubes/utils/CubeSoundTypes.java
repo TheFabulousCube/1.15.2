c0c86e73db382d0f6e4bdc9d8141582e3cc9c19f
Marquardt Snell <thefabulouscube@tadaatiedye.com>
2020-06-19 21:33:33 -0400

    New Graphics

--- src/main/java/com/tadaatiedye/puzzlecubes/utils/CubeSoundTypes.java
+++ src/main/java/com/tadaatiedye/puzzlecubes/utils/CubeSoundTypes.java
@@ -6,8 +6,12 @@ import net.minecraft.block.SoundType;
 
 public class CubeSoundTypes {
 
-	public static final SoundType CUBE = new SoundType(0.8F, 1.0F, SoundInit.BLOCK_CUBE_BREAK,
-			SoundInit.BLOCK_CUBE_STEP, SoundInit.BLOCK_CUBE_PLACE, SoundInit.BLOCK_CUBE_HIT, SoundInit.BLOCK_CUBE_FALL);
+	public static final SoundType CUBE = new SoundType(0.7F, 1.0F, 
+			SoundInit.BLOCK_CUBE_BREAK,
+			SoundInit.BLOCK_CUBE_STEP, 
+			SoundInit.BLOCK_CUBE_PLACE, 
+			SoundInit.BLOCK_CUBE_HIT, 
+			SoundInit.BLOCK_CUBE_FALL);
 
 //	   public static final Material ROCK = (new Material.Builder(MaterialColor.STONE)).requiresTool().build();
 }
